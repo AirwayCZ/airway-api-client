1a35fc247ccf3ebb69fc18672959bd230ff536f5
Martin Krizan <mnohosten@gmail.com>
2021-07-12 16:29:56 +0200

    API - 1.14 - Confirm packages with parcelId list.

---
+++ src/Request/ConfirmParcelByParcelIdListRequest.php
@@ -0,0 +1,39 @@
+<?php
+declare(strict_types=1);
+
+namespace Airway\Partner\Client\Request;
+
+use Airway\Partner\Client\Request;
+use Webmozart\Assert\Assert;
+
+class ConfirmParcelByParcelIdListRequest implements Request
+{
+    /**
+     * @var array
+     */
+    private $parcelIdList;
+
+    public function __construct(
+        array $parcelIdList
+    )
+    {
+        Assert::allIsInstanceOf($parcelIdList, 'string');
+        $this->parcelIdList = $parcelIdList;
+    }
+
+    public function getUrl(): string
+    {
+        return '/parcel/confirm-by-parcel-id-list';
+    }
+
+    public function getMethod(): string
+    {
+        return 'POST';
+    }
+
+    public function getPayload(): string
+    {
+        return json_encode($this->parcelIdList);
+    }
+
+}
